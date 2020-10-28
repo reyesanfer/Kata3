bfa45497780a7e8bedee0d497be75adb0c17e816
Fernando Sanfiel Reyes <72128771+reyesanfer@users.noreply.github.com>
2020-10-28 09:47:36 +0000

    Kata3 v2 (28/10/2020)

--- src/kata3/HistogramDisplay.java
+++ src/kata3/HistogramDisplay.java
@@ -15,9 +15,12 @@ import org.jfree.ui.ApplicationFrame;
  * @author FernandoSanfielReyes
  */
 public class HistogramDisplay extends ApplicationFrame{
+    
+    private final Histogram<String> histogram;
 
-    public HistogramDisplay(String title) {
+    public HistogramDisplay(String title, Histogram histogram) {
         super(title);
+        this.histogram = histogram;
         this.setContentPane(createPanel());
         this.pack();
     }
@@ -34,11 +37,11 @@ public class HistogramDisplay extends ApplicationFrame{
     
     private DefaultCategoryDataset createDataset(){
         DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
-        dataSet.addValue(23, "", "ulpgc.es");
-        dataSet.addValue(17, "", "dis.ulpgc.es");
-        dataSet.addValue(63, "", "gmail.com");
-        dataSet.addValue(32, "", "hotmail.com");
-        dataSet.addValue(3, "", "ull.es");
+        
+        for (String key : histogram.keySet()) {
+            dataSet.addValue(histogram.get(key), "", key);
+        }
+
         return dataSet;
     }
     
--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -7,7 +7,19 @@ package kata3;
 public class Kata3 {
 
     public static void main(String[] args) {
-        HistogramDisplay histogramDisplay = new HistogramDisplay("HISTOGRAM");
+        /**/
+        Histogram <String> histogram = new Histogram();
+        
+        histogram.increment("ulpgc.es");
+        histogram.increment("ull.es");
+        histogram.increment("dis.ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("gmail.com");
+        histogram.increment("ull.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("gmail.com");
+        
+        HistogramDisplay histogramDisplay = new HistogramDisplay("HISTOGRAM", histogram);
         histogramDisplay.execute();
     }
 
